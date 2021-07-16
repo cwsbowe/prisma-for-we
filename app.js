1a047a52f43f919ef32e0ea8ba59dc92fdb12641
Charlie Bowe <charliewsbowe@gmail.com>
2021-07-16 13:42:38 +0100

    Trialed prisma

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
+
+
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 // A `main` function so that you can use async/await
 async function main() {
     // ... you will write your Prisma Client queries here
     console.log("you will write your Prisma Client queries here")
+    // const user = await prisma.user.create({
+    //     data: {
+    //         email: 'elsa@prisma.io',
+    //         name: 'Elsa Prisma',
+    //     },
+    // })
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: 'elsa@prisma.io',
+        },
+    })
+    console.log(user.name)
+
+    const upsertUser = await prisma.user.upsert({
+        where: {
+            email: 'viola@prisma.io',
+        },
+        update: {
+            name: 'Viola the Magnificent',
+        },
+        create: {
+            email: 'viola@prisma.io',
+            name: 'Viola the Failure',
+        },
+    })
+
+    console.log(upsertUser.name)
+
+
 }
 main()
     .catch(e => {
